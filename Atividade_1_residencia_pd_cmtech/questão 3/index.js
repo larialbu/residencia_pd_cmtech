71a179c5af50652d6932a123f028566d0d751a8c
Larissa Albuquerque <105246799+larialbu@users.noreply.github.com>
2022-09-09 19:24:23 -0300

    Add files via upload

---
+++ Atividade_1_residencia_pd_cmtech/questão 3/index.js
@@ -0,0 +1,12 @@
+function temHabilidade(){
+    var skill = prompt("Digite sua skill: ")
+			
+    if(skill.includes('javascript')) {
+       alert(true);
+     } else {
+       alert(false);
+     } 
+  }
+  
+  var skill = ['javascript', 'reactjs', 'React native'];
+  var resultado = temHabilidade();  
---
+++ Atividade_1_residencia_pd_cmtech/questão 5/index.js
@@ -0,0 +1,4 @@
+var nome = prompt('Digite seu nome: ');
+var habilidades = prompt('Digite suas habilidades: ');
+
+alert("O(a) " + nome + " possui as habilidades: " + habilidades);
