93dfd9d4dd4f909023af92201adf13d42c393e84
Larissa Albuquerque <105246799+larialbu@users.noreply.github.com>
2022-09-10 17:06:09 -0300

    Delete Atividade_1_residencia_pd_cmtech directory

--- Atividade_1_residencia_pd_cmtech/questão 1/index.js
+++
@@ -1,7 +0,0 @@
-var rua = prompt('Rua:');
-var numero = prompt('Numero:');
-var bairro = prompt('Bairro:');
-var cidade = prompt('Cidade:');
-var uf = prompt('UF:');
-
-alert('O usuário mora em ' + cidade + " / " + uf + " no bairro " + bairro + " , na rua " + rua + " com nº " + numero + " . ");
--- Atividade_1_residencia_pd_cmtech/questão 5/index.js
+++
@@ -1,4 +0,0 @@
-var nome = prompt('Digite seu nome: ');
-var habilidades = prompt('Digite suas habilidades: ');
-
-alert("O(a) " + nome + " possui as habilidades: " + habilidades);
